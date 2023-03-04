f01fa928b52864d3c83e96edb22d67bdcd09dda0
Bhupendra Madhu <68414570+TheViz02@users.noreply.github.com>
2023-03-04 20:34:47 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,40 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-const prisma = require('../prisma');
+const prisma = require("../prisma");
 
 // Controllers Here
-const { showData, addData } = require('../controller/create');
-
+const {
+  showData,
+  addData,
+  updateData,
+  deleteData,
+} = require("../controller/create");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.status(200);
-    res.json({ title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.status(200);
+  res.json({ title: "Express" });
 });
 
-router.get('/api', (req, res) => {
-    res.json({
-        message: "Hello from express API"
-    });
+router.get("/api", (req, res) => {
+  res.json({
+    message: "Hello from express API",
+  });
 });
 
-router.get('/data', (req, res) => {
-    (async function () {
-        const data = await prisma.data.findMany();
-        res.json({data: data});
-    })();
+router.get("/data", (req, res) => {
+  (async function () {
+    const data = await prisma.data.findMany();
+    res.json({ data: data });
+  })();
 });
 
-router.get('/showData', showData);
+router.get("/showData", showData);
 
-router.post('/addData', addData);
+router.post("/addData", addData);
 
+router.post("/updateData", updateData);
 
+router.delete("/deleteData", deleteData);
 
 module.exports = router;
